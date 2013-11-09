b4032f5ddbc482ec8ba0493b1447a8825729c5d5
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-09 01:53:06 -0500

    Added some details to Project JPanel

--- src/gui/searchpanel/Project.java
+++ src/gui/searchpanel/Project.java
@@ -1,5 +1,9 @@
 package gui.searchpanel;
 
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Project extends JPanel implements Comparable<Project> {
@@ -8,17 +12,34 @@ public class Project extends JPanel implements Comparable<Project> {
 	private String[] users;
 	
 	Project(String projectName/*, int notifications*/){
+		setLayout(null);
+		setBounds(5,5,275,45);
+		setBorder(BorderFactory.createLineBorder(Color.black));
+		
 		this.projectName = projectName;
-		//this.notifications = notifications;
+		setNotificationNum();
+		this.notifications = getNotificationNum();
+		JLabel notification = new JLabel(Integer.toString(this.notifications));
+		notification.setBounds(3,3,20,20);
+		JLabel projName = new JLabel(projectName);
+		projName.setBounds(25,3,120,40);
+		add(notification);
+		add(projName);
 	}
 	public void setUsers(String[] users){
 		this.users = users;
+	}
+	private void setNotificationNum(){
+		//todo
 	}
 	public String getProjectName(){
 		return projectName;
 	}
 	public int getNotificationNum(){
 		return notifications;
+	}
+	public String[] getUsers(){
+		return this.users;
 	}
 	public int compareTo(Project compares) {
 		int compareQuantity = ((Project) compares).getNotificationNum(); 
