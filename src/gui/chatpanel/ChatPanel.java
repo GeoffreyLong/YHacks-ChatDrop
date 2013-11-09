acbf4298d4223f283a5160e62400b3b6c5f51cf2
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-08 23:12:34 -0500

    Implemented the ChatPanel

--- src/gui/chatpanel/ChatPanel.java
+++ src/gui/chatpanel/ChatPanel.java
@@ -3,13 +3,22 @@ package gui.chatpanel;
 import java.awt.Color;
 
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 public class ChatPanel extends JPanel {
 	public ChatPanel(){
 		setBorder(BorderFactory.createLineBorder(Color.black));
 	}
 	public void panelInit(core.objects.Message[] messages){
-		
+		JPanel innerPanel = new JPanel();
+		innerPanel.setLayout(new BoxLayout(innerPanel, BoxLayout.Y_AXIS)); 
+		for (core.objects.Message message : messages){
+			innerPanel.add(new MessagePanel(message));
+		}
+		JScrollPane scroll = new JScrollPane(innerPanel);
+		scroll.setBounds(5,5,775, 480);
+		add(scroll);
 	}
 }
