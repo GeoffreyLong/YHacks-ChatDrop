19d1effb284c5ef705fe1d6e1873072233de085f
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-09 03:56:18 -0500

    Implemented KeyListener

--- src/gui/chatpanel/EntryPanel.java
+++ src/gui/chatpanel/EntryPanel.java
@@ -1,13 +1,15 @@
 package gui.chatpanel;
 
 import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.text.DefaultCaret;
 
-public class EntryPanel extends JPanel{
+public class EntryPanel extends JPanel implements KeyListener{
 	final JTextArea entryArea;
 	public EntryPanel(){
 		setBorder(BorderFactory.createLineBorder(Color.black));
@@ -21,4 +23,17 @@ public class EntryPanel extends JPanel{
 		add(entryArea);
 		setVisible(true);
 	}
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
