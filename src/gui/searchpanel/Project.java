b861892925dcb8ef08cb94d0ffe51b37805f104c
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-09 00:38:49 -0500

    Altered the parameter constructor

--- src/gui/searchpanel/Project.java
+++ src/gui/searchpanel/Project.java
@@ -7,11 +7,14 @@ public class Project extends JPanel{
 	private int notifications;
 	private String[] users;
 	
-	Project(String projectName, int notifications){
+	Project(String projectName/*, int notifications*/){
 		this.projectName = projectName;
-		this.notifications = notifications;
+		//this.notifications = notifications;
 	}
-	public void getUsers(String[] users){
+	public void setUsers(String[] users){
 		this.users = users;
+	}
+	public String getProjectName(){
+		return projectName;
 	}
 }
