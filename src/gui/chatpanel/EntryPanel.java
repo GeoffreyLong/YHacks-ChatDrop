82b5291874fbe55d9f8ed0bb7324d4d18ea8c988
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-09 00:38:10 -0500

    Added text wrapping to the entry textArea

--- src/gui/chatpanel/EntryPanel.java
+++ src/gui/chatpanel/EntryPanel.java
@@ -5,13 +5,18 @@ import java.awt.Color;
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.text.DefaultCaret;
 
 public class EntryPanel extends JPanel{
 	final JTextArea entryArea;
 	public EntryPanel(){
 		setBorder(BorderFactory.createLineBorder(Color.black));
 		entryArea = new JTextArea();
+		DefaultCaret caret = (DefaultCaret)entryArea.getCaret();
+		caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
 		entryArea.setBounds(5,5,775,155);
+		entryArea.setLineWrap(true);
+		entryArea.setWrapStyleWord(true);
 		setLayout(null);
 		add(entryArea);
 		setVisible(true);
