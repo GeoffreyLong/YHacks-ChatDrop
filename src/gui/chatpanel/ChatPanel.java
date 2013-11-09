90c305d0d94b30737b6355fd4b569aaa3b641efe
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-09 02:31:51 -0500

    Will output a different Panel configuration if no chat is entered

--- src/gui/chatpanel/ChatPanel.java
+++ src/gui/chatpanel/ChatPanel.java
@@ -4,19 +4,28 @@ import java.awt.Color;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 public class ChatPanel extends JPanel {
 	public ChatPanel(core.objects.Message[] messages){
-		setBorder(BorderFactory.createLineBorder(Color.black));
-		JPanel innerPanel = new JPanel();
-		innerPanel.setLayout(new BoxLayout(innerPanel, BoxLayout.Y_AXIS)); 
-		for (core.objects.Message message : messages){
-			innerPanel.add(new MessagePanel(message));
+		if (messages == null){
+			JLabel welcomeLabel = new JLabel("Welcome to Chatterbox, Choose a chat to begin");
+			welcomeLabel.setBounds(100,100,300,300);
+			add(welcomeLabel);
 		}
-		JScrollPane scroll = new JScrollPane(innerPanel);
-		scroll.setBounds(5,5,775, 480);
-		add(scroll);
+		else{
+			removeAll();
+			setBorder(BorderFactory.createLineBorder(Color.black));
+			JPanel innerPanel = new JPanel();
+			innerPanel.setLayout(new BoxLayout(innerPanel, BoxLayout.Y_AXIS)); 
+			for (core.objects.Message message : messages){
+				innerPanel.add(new MessagePanel(message));
+			}
+			JScrollPane scroll = new JScrollPane(innerPanel);
+			scroll.setBounds(5,5,775, 480);
+			add(scroll);
+		}
 	}
 }
